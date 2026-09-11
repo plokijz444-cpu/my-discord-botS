91d648f45710cae1d59766060d9ba4df68fa7dd4
plokijz444-cpu <plokijz444@gmail.com>
2026-09-11 19:59:10 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import discord
 from discord.ext import commands
 import datetime
 import re
-intents.message_content = True
 # ================= [ 상단 추가 코드 ] =================
 app = Flask('')
 
@@ -47,6 +46,9 @@ keep_alive()
 intents = discord.Intents.default()
 intents.message_content = True  
 intents.members = True          
+
+intents = discord.Intents.default()
+intents.message_content = True  
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
